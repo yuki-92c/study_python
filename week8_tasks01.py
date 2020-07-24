6776bdd835774f8ee449bb14dc26ee483a56387a
Yuki Saito <jdpmcs@gmail.com>
2020-07-25 08:42:23 +0900

    initial commit

--- week8_tasks01.py
+++ week8_tasks01.py
@@ -10,12 +10,25 @@ df.loc['b','e','g'] = np.nan
 
 # 1. Print the dataframe. What values are missing
 print(df)
+# → b, e, g
 
 # 2. Use .isnull() to find the missing values in the dataframe.
 print(df.isnull())
 
 # 3. Use .notnull() to find the values in the dataframe.
+print(df.notnull())
+
 # 4. Use .sum() on column A. What value do you get?
+# print(df.sum(axis=0))
+print(df['A'].sum())
+
 # 5. Use .fillna() to replace the NaNs with 1. Print your result.
+print(df.fillna(1))
+
 # 6. Use .dropna() to drop the NaN values in the dataframe. Print your result.
+print(df.dropna())
+
 # 7. Replace the value ‘31’ in the dataframe with a 0. (hint: you can google a function for this)
+print(df.replace(31, 0))
+
+
